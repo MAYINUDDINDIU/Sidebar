1be49570688d8f0caa7285bb575223699f7c1a6a
MAYINUDDIN <mainu768971@gmail.com>
2022-10-03 15:02:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
+import { AboutUs, OurAim, OurVision } from "./pages/AboutUs";
+import {
+Services,
+ServicesOne,
+ServicesTwo,
+ServicesThree,
+} from "./pages/Services";
+// import { Events, EventsOne, EventsTwo } from "./pages/Events";
+import Contact from "./pages/ContactUs";
+import Support from "./pages/Support";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+
+    <Router>
+      <Sidebar />
+      <Routes>
+      <Route path="/about-us" element={<AboutUs />}  >About</Route>
+ 	<Route path="/services" element={<Services/>} />
+	 	{/* <Route path="/services/services1" exact component={ServicesOne} />
+ 	<Route path="/services/services2" exact component={ServicesTwo} />
+ 	<Route path="/services/services3" exact component={ServicesThree} /> */}
+		<Route path="/contact" element={<Contact/>} /> 
+		 {/* <Route path="/events" exact component={Events} />
+ 	<Route path="/events/events1" exact component={EventsOne} />
+		<Route path="/events/events2" exact component={EventsTwo} />  */}
+		<Route path="/support" element={<Support/>} />
+      </Routes>
+    </Router>
+
+);
 }
 
 export default App;
